19252c0b489390f889c4c8d2478b4559750ef9bf
Daniel <daniel.javorszky@gmail.com>
2021-04-04 19:29:46 +0200

    Add testing support

---
+++ core/src/test/com/djavorszky/game/GameTemplateTest.kt
@@ -0,0 +1,14 @@
+package com.djavorszky.game
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class GameTemplateTest {
+
+  // TODO Add the tests here
+  @Test
+  fun `test context loads successfully`() {
+    assertThat(2 + 2).isEqualTo(4)
+  }
+
+}
