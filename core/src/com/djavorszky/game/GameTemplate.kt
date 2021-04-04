5ee2b2b626cfb80525add3c623447180f819e701
Daniel <daniel.javorszky@gmail.com>
2021-04-04 18:57:42 +0200

    Change to Kotlin

---
+++ core/src/com/djavorszky/game/GameTemplate.kt
@@ -0,0 +1,37 @@
+package com.djavorszky.game
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.utils.ScreenUtils
+
+// TODO update main entrypoint
+class GameTemplate : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var img: Texture
+
+    private val backgroundColor = Color(.24f, .444f, .67f, 1f)
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(backgroundColor)
+
+        batch.let {
+            it.begin()
+            it.draw(img, 0f, 0f)
+            it.end()
+        }
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
---
+++ desktop/src/com/djavorszky/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,17 @@
+package com.djavorszky.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.djavorszky.game.GameTemplate
+
+fun main(arg: Array<String>) {
+    val config = LwjglApplicationConfiguration()
+
+    config.let {
+        it.title = "Sample Game Template" // TODO update
+        it.width = 1280
+        it.height = 720
+    }
+
+    LwjglApplication(GameTemplate(), config)
+}
